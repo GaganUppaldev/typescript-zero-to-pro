6036c9ade084c4ae833f5a77bb9ab54a916204fd
Gagan Uppal <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-17 14:03:59 +0530

    Update basic.ts
    explained more accurately about js vs ts and how typescript prevent runtime errors by throwing errors during development time

--- basic.ts
+++ basic.ts
@@ -13,3 +13,6 @@ console.log(user.name);
 //here typescript will thow an error that location does not exist in user while javascript will one provide an error 
 //javascript error -> undefined , this will make problem for programmer in big code project to spot the  error
 //console.log(user.location);
+
+//Typescript will throw error during time of development and don't run the code  which will prevent runtime errors
+//But javascript will run the code and which will cause runtime errors
